188d3f3c0ce60083d3fd8619d52aaa8319592fa2
2daw3 <javiasterio@gmail.com>
2021-09-15 13:01:01 +0200

    #4

--- Program.cs
+++ Program.cs
@@ -102,6 +102,23 @@ Console.WriteLine("La media es de aprobados " + MediaArrayMenorDe5(notas));*/
 /////////////////////////////////////////////////////////////////////////////
 Console.WriteLine("--------------------------------------------------------");
 /////////////////////////////////////////////////////////////////////////////
+/*(String,Char,decimal) A1=("Luis",'H',7.5M);
+(String,Char,decimal) A2=("Mara",'M',4M);
+(String,Char,decimal) A3=("Marcos",'H',6M);
+(String,Char,decimal) A4=("Aroa",'M',5M);
+(String,Char,decimal) A5=("Nerea",'M',4M);
+(String,Char,decimal) A6=("Kike",'H',6.5M);
+(String,Char,decimal) A7=("Juan",'H',7M);
+var alumnos = new[] { A1, A2, A3, A4, A5, A6, A7};
+//Console.WriteLine("El alumno " + A1.Item1+ " es " + A1.Item2 + " y la media es de " + A1.Item3);
+decimal media=0;
+for (int i = 0; i < alumnos.Length; i++){
+    media+=alumnos[i].Item3;
+}
+Console.WriteLine("La media de los " + alumnos.Length + " es de " + media/alumnos.Length);*/
+/////////////////////////////////////////////////////////////////////////////
+Console.WriteLine("--------------------------------------------------------");
+/////////////////////////////////////////////////////////////////////////////
 (String,Char,decimal) A1=("Luis",'H',7.5M);
 (String,Char,decimal) A2=("Mara",'M',4M);
 (String,Char,decimal) A3=("Marcos",'H',6M);
@@ -110,3 +127,9 @@ Console.WriteLine("--------------------------------------------------------");
 (String,Char,decimal) A6=("Kike",'H',6.5M);
 (String,Char,decimal) A7=("Juan",'H',7M);
 var alumnos = new[] { A1, A2, A3, A4, A5, A6, A7};
+//Console.WriteLine("El alumno " + A1.Item1+ " es " + A1.Item2 + " y la media es de " + A1.Item3);
+decimal media=0;
+for (int i = 0; i < alumnos.Length; i++){
+    media+=alumnos[i].Item3;
+}
+Console.WriteLine("La media de los " + alumnos.Length + " es de " + media/alumnos.Length);
